6840d681ea07e64303128971d3acce725946dfd9
Adrer05 <adrianlugo2505@gmail.com>
2026-06-20 23:13:34 -0400

    feat: completados modulos de semestre, carreras y asignaturas

--- src/roles/dto/create-rol.dto.ts
+++ src/roles/dto/create-rol.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateRolDto {
 
@@ -7,6 +7,7 @@ export class CreateRolDto {
     name: string;
 
     @IsString()
+    @IsOptional()
     description: string;
 
 }
